073f7d18604190d51702c5e1484c326ffae22fa1
Florian <mrminecrafttv19@gmail.com>
2025-04-05 07:38:32 +0200

    feat: add temp npc registry instead of deleting saves.yml

--- src/main/java/ovh/fedox/flocklobby/util/NPCUtil.java
+++ src/main/java/ovh/fedox/flocklobby/util/NPCUtil.java
@@ -4,7 +4,6 @@ import eu.decentsoftware.holograms.api.DHAPI;
 import eu.decentsoftware.holograms.api.holograms.Hologram;
 import lombok.Getter;
 import lombok.experimental.UtilityClass;
-import net.citizensnpcs.Citizens;
 import net.citizensnpcs.api.CitizensAPI;
 import net.citizensnpcs.api.event.NPCLeftClickEvent;
 import net.citizensnpcs.api.event.NPCRightClickEvent;
@@ -18,20 +17,17 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitTask;
 import org.mineacademy.fo.Common;
 import org.mineacademy.fo.Messenger;
 import ovh.fedox.flockapi.database.RedisManager;
 import ovh.fedox.flockapi.util.SoundUtil;
-import ovh.fedox.flocklobby.FlockLobby;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.exceptions.JedisConnectionException;
 
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
-import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -139,7 +135,8 @@ public class NPCUtil implements Listener {
 
 			lastRequestTimes.put(skinName, currentTime);
 
-			NPC newNPC = CitizensAPI.getNPCRegistry().createNPC(EntityType.PLAYER, npc.getSKIN(), npc.getLOCATION());
+
+			NPC newNPC = CitizensAPI.getTemporaryNPCRegistry().createNPC(EntityType.PLAYER, npc.getSKIN(), npc.getLOCATION());
 
 			newNPC.setName(Common.colorize("&8&m                "));
 			newNPC.getOrAddTrait(LookClose.class).lookClose(true);
@@ -147,9 +144,6 @@ public class NPCUtil implements Listener {
 			SkinTrait skinTrait = newNPC.getOrAddTrait(SkinTrait.class);
 			skinTrait.setSkinName(npc.getSKIN());
 			skinTrait.setShouldUpdateSkins(true);
-
-			newNPC.getEntity().setMetadata(NPC.Metadata.SHOULD_SAVE.getKey(), new FixedMetadataValue(plugin, false));
-
 
 			npcServerMap.put(newNPC.getId(), npc.getSERVER().toLowerCase());
 
@@ -343,8 +337,6 @@ public class NPCUtil implements Listener {
 			Common.log("&cError accessing NPC registries: " + e.getMessage());
 		}
 
-		deleteSavesDirectory();
-
 		npcs.clear();
 		npcServerMap.clear();
 		lastRequestTimes.clear();
@@ -356,26 +348,6 @@ public class NPCUtil implements Listener {
 				Common.log("&cError cancelling update task: " + e.getMessage());
 			}
 			updateTask = null;
-		}
-	}
-
-	/**
-	 * Deletes the Citizens/saves.yml directory, im doing this cause im too dumb to using their API
-	 */
-	private void deleteSavesDirectory() {
-		try {
-			String citizensFolder = FlockLobby.getPlugin(Citizens.class).getDataFolder().getAbsolutePath();
-			File savesFile = new File(citizensFolder, "saves.yml");
-
-			if (savesFile.exists()) {
-				savesFile.delete();
-				Common.log("&aDeleted Citizens/saves.yml file.");
-			} else {
-				Common.log("&cCitizens/saves.yml file does not exist.");
-			}
-
-		} catch (Exception e) {
-			Common.log("&cError deleting Citizens/saves.yml file: " + e.getMessage());
 		}
 	}
 
